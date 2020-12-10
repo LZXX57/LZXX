9ed8fb5b27b1aaed06b5326d95ac0a56cd79c79a
jinfeihan57@gmail.com <jinfeihan57@gmail.com>
2020-12-10 20:57:42 +0800

    update inflate struct

--- lib/deflate/common.h
+++ lib/deflate/common.h
@@ -10,20 +10,22 @@
 extern "C" {
 #endif
 
-#ifndef ZEXTERN
-#  define ZEXTERN extern
+#ifndef HEXTERN
+#  define HEXTERN extern
 #endif
 
 typedef void* voidpf;         /* void * void_ptr */
 typedef unsigned char  Byte;  /* 8 bits */
+typedef unsigned char  uInt8;  /* 8 bits */
 typedef unsigned int   uInt;  /* 16 bits or more */
 typedef unsigned long  uLong; /* 32 bits or more */
+typedef unsigned long long uLong; /* 32 bits or more */
 
 typedef voidpf (*alloc_func) (voidpf opaque, uInt items, uInt size);
 typedef void   (*free_func)  (voidpf opaque, voidpf address);
 struct internal_state;
 
-typedef struct z_stream_s {
+typedef struct h_stream_s {
     const Byte *next_in;     /* next input byte */
     uInt     avail_in;       /* number of bytes available at next_in */
     uLong    total_in;       /* total number of input bytes read so far */
@@ -43,9 +45,37 @@ typedef struct z_stream_s {
                            for deflate, or the decoding state for inflate */
     uLong   adler;      /* Adler-32 or CRC-32 value of the uncompressed data */
     uLong   reserved;   /* reserved for future use */
-} z_stream;
+} h_stream;
 
-typedef z_stream* z_streamp;
+typedef h_stream* h_streamptr;
+
+typedef enum {
+    HEAD = 0,           // 解析 3bit 头
+    COPY,               // copy 模式
+    HLIT,               // 字符和匹配长度树sym数 5bits （257-286）
+    HDIST,              // 偏移距离树sym数 5bits （1-32）
+    HCLEN,              // 游程树sym数 4bits （4-19）
+    RUNLTREE,           // 解码 游程树
+    RUNL,               // 解码游程数据
+    LLTREE,             // 解码 字符和匹配长度树
+    LENEXT,             // 匹配长度额外bit
+    DTREE,              // 解码偏移量树
+    DEXT,               // 偏移距离额外bit
+    DATA_STREAM         // 解码压缩数据流
+} h_state;
+
+typedef enum {
+    CONTINUE_BLOCK = 0,           // 非最后block
+    LAST_BLOCK                    // 最后block
+} h_block_type;
+
+typedef struct h_tree_s{
+    uInt counts[16];  // 记录各个huffman码长的个数，huffman码长最大不会超过15bits
+    uInt symbols[288]; // 三棵huffman树最大的即为静态 lltree 288个叶子节点（dtree较小，动态树小于等于静态树）
+    uInt max_symbol;   // 记录最大的 symbol 大于最大symbol的symbols索引都无效
+} h_tree;
+
+typedef h_tree* h_treePtr;
 
 #ifdef __cplusplus
 }
--- lib/deflate/inflate.h
+++ lib/deflate/inflate.h
@@ -1,4 +1,5 @@
 /*********************************************************
+ * 
  * inflate.h -- internal inflate state definition
  * 
 **********************************************************/
@@ -13,65 +14,53 @@ extern "C" {
 #endif
 
 /* inflate init z_stream struct construct */
-ZEXTERN int inflateInit(z_streamp strm);
+HEXTERN int inflateInit(h_streamptr strm);
 
 /* inflate decompress */
-ZEXTERN int inflate(z_streamp strm, int flush);
+HEXTERN int inflate(h_streamptr strm, int flush);
 
 /* inflate end z_stream struct deconstruct */
-ZEXTERN int inflateEnd(z_streamp strm);
+HEXTERN int inflateEnd(h_streamptr strm);
 
 struct inflate_state {
     // 保证文件在解压不满一个字节时，或者跨字节边界解压又刚好到达输入文件末尾时能记录信息
-    // copy 存储时的信息记录
-    // 记录当前解压类型 游程编码，lltree，dtree，data_stream 不同解压内容策略不同
+    h_streamptr strm; // 指向输入输出流及内存申请函数
+    h_block_type block_type; // block的类型
+    h_state state; // 记录当前解压类型 copy 游程编码，lltree，dtree，data_stream 不同解压内容策略不同
+    
+    struct copy_mode{       // copy模式对象属性
+        uInt8 block_length;  // 记录 长度 4 Byte读取情况
+        uInt block_size;    // 记录block大小，不会超过65535
+        uInt block_nsize;   // block_size补码
+        uInt block_decompress_size; // 记录已解压的block大小
+    };
+    struct copy_mode copy_state;
+
+    struct compress_mode{           // 压缩模式对象属性
+        uInt8 lltree_syms;          // 字符和匹配长度sym数
+        uInt8 lltree_decode_len;    // 字符和匹配长度sym解码数
+        h_treePtr lltree;           // 字符和匹配长度huffman树
+        uInt8 dtree_syms;           // 偏移距离sym数
+        uInt8 dtree_decode_len;     // 偏移距离sym解码数
+        h_treePtr dtree;            // 偏于距离huffman树
+        uInt8 runltree_decode_len;  // 游程树字符sym解码数
+        h_treePtr runltree;         // 游程编码树
+        uInt remain_value;          // 剩余值(当前input解码完但是并没有解码到最后，输入为部分压缩文件，因此需要记录未解压的最后剩余的数据，方便和新的输入一起解压)
+        uInt bits;                  // 剩余bit数
+    };
+    struct compress_mode compress_state;
+
+    struct window_buf{
+        Byte *window;       // 输出缓存区 不小于32K，满足偏移量的最大搜寻距离 （每次扩大为上一次的两倍？？）
+        uLong capacity;     // window 实际空间
+        uLong size;         // window 已使用空间
+    };
+    struct window_buf window;
+
     // 记录各种解压类型的 解码信息
     // 记录输出数据的缓存及输出数据的当前位置，方便 lz77 跨输入的解码
-    // 记录范式huffman树解码结构 三棵huffman树，动静态lltree，动静态dtree，游程编码tree
-    // 解压文件当前大小
-    // 上级结构体指针用于获取和输出inbuf和outbuf
-    // z_streamp strm;             /* pointer back to this zlib stream */
-    // inflate_mode mode;          /* current inflate mode */
-    // int last;                   /* true if processing last block */
-    // int wrap;                   /* bit 0 true for zlib, bit 1 true for gzip,
-    //                                bit 2 true to validate check value */
-    // int havedict;               /* true if dictionary provided */
-    // int flags;                  /* gzip header method and flags (0 if zlib) */
-    // unsigned dmax;              /* zlib header max distance (INFLATE_STRICT) */
-    // unsigned long check;        /* protected copy of check value */
-    // unsigned long total;        /* protected copy of output count */
-    // gz_headerp head;            /* where to save gzip header information */
-    //     /* sliding window */
-    // unsigned wbits;             /* log base 2 of requested window size */
-    // unsigned wsize;             /* window size or zero if not using window */
-    // unsigned whave;             /* valid bytes in the window */
-    // unsigned wnext;             /* window write index */
-    // unsigned char FAR *window;  /* allocated sliding window, if needed */
-    //     /* bit accumulator */
-    // unsigned long hold;         /* input bit accumulator */
-    // unsigned bits;              /* number of bits in "in" */
-    //     /* for string and stored block copying */
-    // unsigned length;            /* literal or length of data to copy */
-    // unsigned offset;            /* distance back to copy string from */
-    //     /* for table and code decoding */
-    // unsigned extra;             /* extra bits needed */
-    //     /* fixed and dynamic code tables */
-    // code const FAR *lencode;    /* starting table for length/literal codes */
-    // code const FAR *distcode;   /* starting table for distance codes */
-    // unsigned lenbits;           /* index bits for lencode */
-    // unsigned distbits;          /* index bits for distcode */
-    //     /* dynamic table building */
-    // unsigned ncode;             /* number of code length code lengths */
-    // unsigned nlen;              /* number of length code lengths */
-    // unsigned ndist;             /* number of distance code lengths */
-    // unsigned have;              /* number of code lengths in lens[] */
-    // code FAR *next;             /* next available space in codes[] */
-    // unsigned short lens[320];   /* temporary storage for code lengths */
-    // unsigned short work[288];   /* work area for code table building */
-    // code codes[ENOUGH];         /* space for code tables */
-    // int sane;                   /* if false, allow invalid distance too far */
-    // int back;                   /* bits back of last unprocessed length/lit */
-    // unsigned was;               /* initial length of match */
+    // 解压文件当前大小 交给上一级 h_streamptr strm; 记录
+    
 };
 
 #ifdef __cplusplus
